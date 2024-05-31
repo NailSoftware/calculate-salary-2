a8ed56efd864cd8687fc6d8117c31e8027be6510
NailSoftware <154961215+NailSoftware@users.noreply.github.com>
2024-05-31 10:07:18 +0400

    Update CalculateSalary.java
    Update

--- src/main/java/CalculateSalary.java
+++ src/main/java/CalculateSalary.java
@@ -7,7 +7,7 @@ public class CalculateSalary {
         int percentage = 8;
         salary -= (salary * percentage) / 100;
 
-        Predicate<Double> predicate = p-> p > 950;
+        Predicate<Double> predicate = p-> p > 950; // 950 yox 1000 yaz
 
         return predicate.test(salary);
 
