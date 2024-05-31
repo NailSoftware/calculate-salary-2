1e7015ebf56b3c4c8437c9293822f67a767ed2ae
feridmustafayev34 <feridmustafayev34@gmail.com>
2024-05-31 10:00:58 +0400

    CalculateSalary class added

---
+++ src/main/java/CalculateSalary.java
@@ -0,0 +1,16 @@
+import java.util.function.Predicate;
+
+public class CalculateSalary {
+
+    public boolean calculate(double salary) {
+
+        int percentage = 8;
+        salary -= (salary * percentage) / 100;
+
+        Predicate<Double> predicate = p-> p > 950;
+
+        return predicate.test(salary);
+
+    }
+
+}
